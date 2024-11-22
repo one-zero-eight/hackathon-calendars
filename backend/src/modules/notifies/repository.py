1e18a8fe313f0528619d2ae4fde6c205d62a39c7
Albert Avkhadeev <albertavkhadeev@gmail.com>
2024-11-23 02:02:36 +0300

    fix: small chng

--- backend/src/modules/notifies/repository.py
+++ backend/src/modules/notifies/repository.py
@@ -1,16 +1,16 @@
-from beanie import PydanticObjectID
+from beanie import PydanticObjectId
 
 from src.modules.notifies.scheams import NotificationCreate
 from src.storages.mongo.notifies import Notification
 
 
 class NotificationRepository:
-    async def create_notification(self, notification_data: NotificationCreate) -> PydanticObjectID:
+    async def create_notification(self, notification_data: NotificationCreate) -> PydanticObjectId:
         notification = Notification(**notification_data.model_dump())
         await notification.insert()
         return notification.id
 
-    async def get_notification(self, notification_id: PydanticObjectID) -> Notification:
+    async def get_notification(self, notification_id: PydanticObjectId) -> Notification:
         return await Notification.get(notification_id)
 
 
--- backend/src/modules/notifies/routes.py
+++ backend/src/modules/notifies/routes.py
@@ -1,4 +1,4 @@
-from beanie import PydanticObjectID
+from beanie import PydanticObjectId
 from fastapi import APIRouter, HTTPException
 
 from src.api.exceptions import IncorrectCredentialsException
@@ -15,12 +15,12 @@ router = APIRouter(
 
 
 @router.post("/")
-async def create_notification(notification_create: NotificationCreate) -> PydanticObjectID:
+async def create_notification(notification_create: NotificationCreate) -> PydanticObjectId:
     return await notification_repository.create_notification(notification_create)
 
 
 @router.get("/{notification_id}")
-async def get_notification(notification_id: PydanticObjectID):
+async def get_notification(notification_id: PydanticObjectId):
     notification = await notification_repository.get_notificationч(notification_id)
     if not notification:
         raise HTTPException(status_code=404, detail="Notification not found")
