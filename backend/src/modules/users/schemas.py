5866b987fb83eacb3ac2d0aeaea92833d4a57a16
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2024-11-22 10:37:26 +0300

    feat: init backend

---
+++ backend/src/modules/users/schemas.py
@@ -0,0 +1,18 @@
+__all__ = ["CreateUser", "ViewUser"]
+
+from beanie import PydanticObjectId
+
+from src.pydantic_base import BaseSchema
+
+
+class CreateUser(BaseSchema):
+    login: str
+    password: str
+
+
+class ViewUser(BaseSchema):
+    id: PydanticObjectId
+
+
+class UserAuthData(BaseSchema):
+    user_id: PydanticObjectId
