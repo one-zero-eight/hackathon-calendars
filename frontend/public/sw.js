70e809740f8d7f5a0f292c73ffaf9e40cd069fcf
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-11-24 02:07:43 +0300

    chore: add log to service worker

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,5 +1,6 @@
 self.addEventListener("push", (event) => {
   const data = event.data;
   const pushMessage = data.json();
+  console.log(event, pushMessage);
   event.waitUntil(self.registration.showNotification(pushMessage.message));
 });
